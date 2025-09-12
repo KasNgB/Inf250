c49c504bc79b948f69e1b2cb646464b86cbee0ba
KasNgB <kasperngbergan@Nei.localdomain>
2025-09-12 11:07:51 +0200

    done

--- Mand_Exercise_1/Outsu_sceleton.py
+++ Mand_Exercise_1/Outsu_sceleton.py
@@ -5,12 +5,12 @@ Skeleton for first part of the blob-detection coursework as part of INF250
 at NMBU (Autumn 2017).
 """
 
-__author__ = "Yngve Mardal Moe"
-__email__ = "yngve.m.moe@gmail.com"
+__author__ = "Kasper Ng Bergan"
+__email__ = "kasper.ng.bergan@nmbu.no"
 
 import numpy as np
 import cv2
-import matplotlib.pyplot as plt
+
 
 img_path = 'gingerbread.jpg'
 gingerbread = cv2.imread(img_path)
@@ -44,8 +44,8 @@ def threshold(image, th=None):
     elif len(shape) > 3:
         raise ValueError('Must be at 2D image')
 
-    # if th is None:
-    #     th = otsu_thval(image)
+    if th is None:
+        th = otsu(image)
 
     # Start thresholding
     ## WRITE YOUR CODE HERE
@@ -81,8 +81,31 @@ def otsu(image):
     """Finds the optimal thresholdvalue of given image using Otsu's method.
     """
     hist = histogram(image)
-    th = 0
+    final_bcv = 0
+    th_final = 0
 
     ## WRITE YOUR CODE HERE
+    total = sum(hist)
+    for th in range(len(hist)):
 
-    return th
+        hist_sum0 = sum(hist[:th])
+        hist_sum1 = sum(hist[th:])
+
+        if hist_sum0 == 0 or hist_sum1 == 0:
+            continue
+
+        omega0 = hist_sum0/total
+        mean0 = sum(i * hist[i] for i in range(th)) / hist_sum0
+
+        omega1 = hist_sum1/total
+        mean1 = sum(i * hist[i] for i in range(th, len(hist))) / hist_sum1
+
+        bcv = np.sqrt(omega0*omega1 * (mean1 - mean0)**2) 
+
+        if bcv > final_bcv:
+            final_bcv = bcv
+            th_final = th
+
+    return th_final
+
+
